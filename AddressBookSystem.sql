3dea1748fbf370432282366c19ac133a49a190b2
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-28 22:10:55 +0530

    [NISHANT] add : Ability to insert new contacts to address book

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -18,4 +18,12 @@ EmailID                    Varchar(150) NOT NULL,
 PRIMARY KEY (PhoneNumber)
 );
 
+# UC3 - Ability to insert new contacts to address book
+INSERT INTO address_book(FirstName,LastName,Address,City,State,ZIP,PhoneNumber,EmailID) VALUES
+ ('Nishant','Chauhan','Chainpur','Vaishali','Bihar',844118,9876543210,'nishant@gmail.com'),
+ ('Anshu','Singh','Goraul','Bhagwanpur','Jharkhand',831014,9988776655,'anshu@gmail.com'),
+ ('Narendra','Modi','Center','Delhi','New Delhi',887766,9988775544,'modi@gmail.com');
+ SELECT * FROM address_book;
+
+
 
