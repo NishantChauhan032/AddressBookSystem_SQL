12884b9f29e1fc84cdb6165d7018b5a1fb281c60
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-28 19:05:33 +0530

    [NISHANT] add : ability to create addressbook table with its attributes

--- AddressBookSystem.sql
+++ AddressBookSystem.sql
@@ -4,3 +4,18 @@ create database AddressBookService;
 use addressbookservice;
 select database();
 
+# UC2 - To create address book table with attributes
+create table address_book
+(
+FirstName                   Varchar(150) NOT NULL,
+LastName                   Varchar(150) NOT NULL,
+Address                    Varchar(250) NOT NULL,
+City                       Varchar(150) NOT NULL,
+State                      Varchar(150) NOT NULL,
+ZIP                        Varchar(150) NOT NULL,
+PhoneNumber                Varchar(150) NOT NULL,
+EmailID                    Varchar(150) NOT NULL,
+PRIMARY KEY (PhoneNumber)
+);
+
+
